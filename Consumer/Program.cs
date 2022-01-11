03da0f4e22d84db24f4cc8d65acf0ecae502e8bd
cpr <cpr@voxteneo.com>
2022-01-11 11:55:44 +0700

    Initial Commit NET 5

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -2,79 +2,85 @@
 using Confluent.Kafka;
 using Confluent.Kafka.Admin;
 using Kafka.Public;
-using Kafka.Public.Loggers;
-ClusterClient _cluster;
-IProducer<Null, string> _producer;
-const string server = "172.17.224.212:9093";
-Console.Write("Masukan nama topic : ");
-var topic = Console.ReadLine();
-string _topic = topic;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
-var config = new ConsumerConfig
+namespace Test
 {
-    BootstrapServers = server,
-    AutoOffsetReset = AutoOffsetReset.Latest,
-    EnableAutoCommit = false,
-    EnableAutoOffsetStore = false,
-    GroupId ="foo"
-};
-PartitionMetadata partitions;
-using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = server }).Build())
-{
-    var meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
-
-    var currentTopic = meta.Topics.SingleOrDefault(t => t.Topic == topic);
-    if (currentTopic == null)
+    internal class Program
     {
-        await adminClient.CreateTopicsAsync(new TopicSpecification[] {
-                    new TopicSpecification { Name = topic, ReplicationFactor = 1, NumPartitions = 3 } });
-        meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
-        currentTopic = meta.Topics.SingleOrDefault(t => t.Topic == topic);
-    }
-    partitions = currentTopic.Partitions?.LastOrDefault();
-
-}
-var _consumer = new ConsumerBuilder<string, string>(config).Build();
-//_consumer.Subscribe(_topic);
-Partition p = new Partition(partitions.PartitionId);
-TopicPartition tp = new TopicPartition(topic, p);
-var offset = 0;
-if (File.Exists("latest"))
-{
-    offset = Convert.ToInt32(File.ReadAllText("latest")) + 1;
-}
-Confluent.Kafka.Offset o = new Confluent.Kafka.Offset(offset);
-TopicPartitionOffset tpo = new TopicPartitionOffset(tp, o);
-_consumer.Subscribe(topic);
-//_consumer.Assign(tpo);
-var cancelled = false;
-while (!cancelled)
-{
-    try
-    {
-        var consumeResult = _consumer.Consume();
-        Console.WriteLine($"{consumeResult.Offset} Received : {consumeResult.Value}");
-        if (int.Parse(consumeResult.Value) % 2 == 0)
+        static async Task Main(string[] args)
         {
-            File.WriteAllText("latest", consumeResult.Offset.ToString());
+
+            ClusterClient _cluster;
+            IProducer<Null, string> _producer;
+            const string server = "172.17.224.212:9093";
+            Console.Write("Masukan nama topic : ");
+            var topic = Console.ReadLine();
+            string _topic = topic;
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = server,
+                AutoOffsetReset = AutoOffsetReset.Latest,
+                EnableAutoCommit = false,
+                EnableAutoOffsetStore = false,
+                GroupId = "foo"
+            };
+            PartitionMetadata partitions;
+            using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = server }).Build())
+            {
+                var meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
+
+                var currentTopic = meta.Topics.SingleOrDefault(t => t.Topic == topic);
+                if (currentTopic == null)
+                {
+                    await adminClient.CreateTopicsAsync(new TopicSpecification[] {
+                    new TopicSpecification { Name = topic, ReplicationFactor = 1, NumPartitions = 3 } });
+                    meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
+                    currentTopic = meta.Topics.SingleOrDefault(t => t.Topic == topic);
+                }
+                partitions = currentTopic.Partitions?.LastOrDefault();
+
+            }
+            var _consumer = new ConsumerBuilder<string, string>(config).Build();
+            //_consumer.Subscribe(_topic);
+            Partition p = new Partition(partitions.PartitionId);
+            TopicPartition tp = new TopicPartition(topic, p);
+            var offset = 0;
+            if (File.Exists("latest"))
+            {
+                offset = Convert.ToInt32(File.ReadAllText("latest")) + 1;
+            }
+            Confluent.Kafka.Offset o = new Confluent.Kafka.Offset(offset);
+            TopicPartitionOffset tpo = new TopicPartitionOffset(tp, o);
+            _consumer.Subscribe(topic);
+            //_consumer.Assign(tpo);
+            var cancelled = false;
+            while (!cancelled)
+            {
+                try
+                {
+                    var consumeResult = _consumer.Consume();
+                    Console.WriteLine($"{consumeResult.Offset} Received : {consumeResult.Value}");
+                    if (int.Parse(consumeResult.Value) % 2 == 0)
+                    {
+                        File.WriteAllText("latest", consumeResult.Offset.ToString());
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+                // handle consumed message.
+            }
+
+            _consumer.Close();
         }
-
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
-  
-
-    // handle consumed message.
-}
-
-_consumer.Close();
-
-
-
-Task StopAsync(CancellationToken cancellationToken)
-{
-    _cluster?.Dispose();
-    return Task.CompletedTask;
 }
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,78 +1,96 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Confluent.Kafka;
 using Confluent.Kafka.Admin;
-const string server = "172.17.224.212:9093";
-Console.Write("Masukan nama topic : ");
-var topic = Console.ReadLine();
-var config = new ProducerConfig()
-{
-    BootstrapServers = server,
-    MessageSendMaxRetries = 1,
-    RetryBackoffMs = 1000,
-    Acks = Acks.All,
-    RequestTimeoutMs = 2000,
-    SocketTimeoutMs = 2500,
-    TransactionTimeoutMs = 1500,
-    MaxInFlight = 1,
-    LingerMs = 5,
-    EnableDeliveryReports = true,
-    EnableIdempotence = false
-};
-var _producer = new ProducerBuilder<Null, string>(config).Build();
-while (true)
-{
-    Console.Write("Masukan angka yang akan di kirim : ");
-    var angka = Console.ReadLine();
-    if (angka.StartsWith("p"))
-        await SendWithPartition(angka.Substring(1));
-    else
-        await SendWithouPartition(angka);
+using Kafka.Public;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
-
-}
-
-async Task SendWithPartition(string? angka)
+namespace Test
 {
-    List<PartitionMetadata> partitions;
-    using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = server }).Build())
+    internal class Program
     {
-        var meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
-
-        var currentTopic = meta.Topics.SingleOrDefault(t => t.Topic == topic);
-        if (currentTopic == null)
+        const string server = "172.17.224.212:9093";
+        static string topic;
+        static IProducer<Null, string> _producer;
+        static async Task SendWithPartition(string? angka)
         {
-            await adminClient.CreateTopicsAsync(new TopicSpecification[] {
-                    new TopicSpecification { Name = topic, ReplicationFactor = 1, NumPartitions = 3 } });
-            meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
-            currentTopic = meta.Topics.SingleOrDefault(t => t.Topic == topic);
-        }
-        partitions = currentTopic.Partitions;
-        foreach (var partition in partitions)
-        {
-            var result = await _producer.ProduceAsync(new TopicPartition(topic, new Partition(partition.PartitionId)), new Message<Null, string>()
+            List<PartitionMetadata> partitions;
+            using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = server }).Build())
             {
-                Value = angka
-            });
-            Console.WriteLine(result.Status);
+                var meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
+
+                var currentTopic = meta.Topics.SingleOrDefault(t => t.Topic == topic);
+                if (currentTopic == null)
+                {
+                    await adminClient.CreateTopicsAsync(new TopicSpecification[] {
+                    new TopicSpecification { Name = topic, ReplicationFactor = 1, NumPartitions = 3 } });
+                    meta = adminClient.GetMetadata(TimeSpan.FromSeconds(20));
+                    currentTopic = meta.Topics.SingleOrDefault(t => t.Topic == topic);
+                }
+                partitions = currentTopic.Partitions;
+                foreach (var partition in partitions)
+                {
+                    var result = await _producer.ProduceAsync(new TopicPartition(topic, new Partition(partition.PartitionId)), new Message<Null, string>()
+                    {
+                        Value = angka
+                    });
+                    Console.WriteLine(result.Status);
+                }
+
+            }
         }
 
-    }
-}
-
-async Task SendWithouPartition(string? angka)
-{
-    try
-    {
-        var result = await _producer.ProduceAsync(topic, new Message<Null, string>()
+        static async Task SendWithouPartition(string? angka)
         {
-            Value = angka
-        });
-        Console.WriteLine(result.Status);
+            try
+            {
+                var result = await _producer.ProduceAsync(topic, new Message<Null, string>()
+                {
+                    Value = angka
+                });
+                Console.WriteLine(result.Status);
 
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex);
-    }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
+        }
+        static async Task Main(string[] args)
+        {
+
+            Console.Write("Masukan nama topic : ");
+            topic = Console.ReadLine();
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = server,
+                MessageSendMaxRetries = 1,
+                RetryBackoffMs = 1000,
+                Acks = Acks.All,
+                RequestTimeoutMs = 2000,
+                SocketTimeoutMs = 2500,
+                TransactionTimeoutMs = 1500,
+                MaxInFlight = 1,
+                LingerMs = 5,
+                EnableDeliveryReports = true,
+                EnableIdempotence = false
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+            while (true)
+            {
+                Console.Write("Masukan angka yang akan di kirim : ");
+                var angka = Console.ReadLine();
+                if (angka.StartsWith("p"))
+                    await SendWithPartition(angka.Substring(1));
+                else
+                    await SendWithouPartition(angka);
+
+
+            }
+        }
+    }
 }
