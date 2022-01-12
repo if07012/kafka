a6431d18588bcc5c51dcb692253f95a16c530f8a
cpr <cpr@voxteneo.com>
2022-01-12 16:49:46 +0700

    Initial Commit GRPC

---
+++ Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Server;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                //using var channel = GrpcChannel.ForAddress("https://localhost:7178");
+                var channel = new Channel("localhost:5223", Grpc.Core.ChannelCredentials.Insecure);
+                var client = new Greeter.GreeterClient(channel);
+                var reply = await client.SayHelloAsync(
+                                  new HelloRequest { Name = "GreeterClient" });
+                Console.WriteLine("Greeting: " + reply.Message);
+            }
+
+        }
+    }
+}
---
+++ Server/GrpcHostedServer.cs
@@ -0,0 +1,30 @@
+﻿using Server;
+using Server.Services;
+
+internal class GrpcHostedServer : IHostedService
+{
+    private readonly ILogger<GreeterService> logger;
+    Grpc.Core.Server _server = null;
+    int port = 5223;
+    public GrpcHostedServer(ILogger<GreeterService> logger)
+    {
+        this.logger = logger;
+    }
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _server = new Grpc.Core.Server()
+        {
+            Services = { Greeter.BindService(new Server.Services.GreeterService(logger)) },
+            Ports = { new Grpc.Core.ServerPort("localhost", port, Grpc.Core.ServerCredentials.Insecure) }
+
+        };
+        Console.WriteLine("Start Grpc");
+        _server.Start();
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return _server.ShutdownAsync();
+    }
+}
